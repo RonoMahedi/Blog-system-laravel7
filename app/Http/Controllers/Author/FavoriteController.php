c5bdb28946c8bfec4ac00fcb59c205db1ac3d87e
Abul Kalam Azad <45317527+RonoMahedi@users.noreply.github.com>
2020-05-17 23:47:25 +0600

    Update FavoriteController.php

--- app/Http/Controllers/Author/FavoriteController.php
+++ app/Http/Controllers/Author/FavoriteController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\Author;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-  $posts = Auth::user()->favorite_posts;
-  return view('author.favorite',compact('posts'));
+    public function index()
+    {
+        $posts = Auth::user()->favorite_posts;
+        return view('author.favorite',compact('posts'));
+    }
 }
